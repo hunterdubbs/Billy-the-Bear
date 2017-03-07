c96b58c199f255f37a6f9d1c64701412c9ec3978
nugner <hdubbs77@gmail.com>
2017-03-07 07:48:10 -0500

    confirmed working with hotfixes
    This is minimal functional version

--- ticTacToe.ino
+++ ticTacToe.ino
@@ -15,10 +15,12 @@ int pinCounter = 22;
 
 //turn number; used for optimization
 int turn = 0;
+int feedbackCounter = 1;
 
 const int SEPERATER_CONSTANT = 9;
 
 void setup() {
+  Serial.begin(9600);
   //assign pins******
   //buttons
   for(int i=0;i<9;i++){
@@ -33,11 +35,61 @@ void setup() {
   }
       
   //************ setup additions can go here*******************
-
+  turnIdentifier = 1;
   
 }
 
 void loop() {
+  //debugging
+  /*
+  for(int i=2;i<11;i++){
+    if(digitalRead(i) == HIGH){
+      Serial.println(i);
+    }
+  }*/
+
+  /*board[0][0] = 1;
+  board[0][1] = 1;
+  board[0][2] = 1;
+  board[1][0] = 1;
+  board[1][1] = 1;
+  board[1][2] = 1;
+  board[2][0] = 1;
+  board[2][1] = 1;
+  board[2][2] = 1;
+  updateLED();
+
+
+  delay(3000);
+
+  board[0][0] = 0;
+  board[0][1] = 0;
+  board[0][2] = 0;
+  board[1][0] = 0;
+  board[1][1] = 0;
+  board[1][2] = 0;
+  board[2][0] = 0;
+  board[2][1] = 0;
+  board[2][2] = 0;
+  updateLED();
+
+  delay(3000);
+
+  board[0][0] = 2;
+  board[0][1] = 2;
+  board[0][2] = 2;
+  board[1][0] = 2;
+  board[1][1] = 2;
+  board[1][2] = 2;
+  board[2][0] = 2;
+  board[2][1] = 2;
+  board[2][2] = 2;
+  updateLED();
+
+  delay(3000);*/
+
+  //end debugging
+  
   if (turnIdentifier == 1){
     //check button states
     int pinCounter = 2;
@@ -52,23 +104,63 @@ void loop() {
           }else{
             turnIdentifier = 1;
           }
+          Serial.println("player turn accepted");
           updateLED();
-          checkForWin();
+          if(checkForWin() == false){
+            if(turn >= 9){
+              //was a draw
+              //reset
+              board[0][0] = 0;
+              board[0][1] = 0;
+              board[0][2] = 0;
+              board[1][0] = 0;
+              board[1][1] = 0;
+              board[1][2] = 0;
+              board[2][0] = 0;
+              board[2][1] = 0;
+              board[2][2] = 0;
+              updateLED();
+              turn = 0;
+              turnIdentifier = 1;
+              delay(3000);
+            }
+          }
         }
         pinCounter++;
       }
     }
   }
+  
    //computer's turn
    if (turnIdentifier == 2){
     bool turnComplete = false;
-    while (!turnComplete){
-     int col = floor(random(0, 2.999));
-     int row = floor(random(0, 2.999));
+    while (turnComplete == false){
+     int col = floor(random(0, 3.999));
+     int row = floor(random(0, 3.999));
      if(board[col][row] == 0){
       board[col][row] = 2;
+      Serial.println("AI turn accepted");
       updateLED();
-      checkForWin();
+      turnIdentifier = 1;
+      if(checkForWin() == false){
+            if(turn >= 9){
+              //was a draw
+              //reset
+              board[0][0] = 0;
+              board[0][1] = 0;
+              board[0][2] = 0;
+              board[1][0] = 0;
+              board[1][1] = 0;
+              board[1][2] = 0;
+              board[2][0] = 0;
+              board[2][1] = 0;
+              board[2][2] = 0;
+              updateLED();
+              turn = 0;
+              turnIdentifier = 1;
+              delay(3000);
+            }
+        }
       turn++;
       turnComplete = true;
      }
@@ -85,61 +177,69 @@ void setLED(int col, int row, int color){
 }
 
 void updateLED(){
+  int pinCounter = 22;
+  int pinCounterAI = 31;
   for (int i = 0; i < 3; i++){
     for (int j= 0; j< 3; j++){
-      
       if (board[i][j] == 0){
         digitalWrite(pinCounter, LOW);
-        digitalWrite(pinCounter + SEPERATER_CONSTANT, LOW);
+        digitalWrite(pinCounterAI, LOW);
       }
       else if (board[i][j] == 1){
         digitalWrite(pinCounter, HIGH);
-        digitalWrite(pinCounter + SEPERATER_CONSTANT, LOW);
+        digitalWrite(pinCounterAI, LOW);
       }
       else if (board[i][j] == 2){
         digitalWrite(pinCounter, LOW);
-        digitalWrite(pinCounter + SEPERATER_CONSTANT, HIGH);
+        digitalWrite(pinCounterAI, HIGH);
       }
       pinCounter++;
+      pinCounterAI++;
     }
   }
 }
 
-void checkForWin(){
+bool checkForWin(){
   //see if it is yet possible to win
   if(turn >= 5){
   //player wins **********
   //up - down
-    if(board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1) {playerWin("c", 0);}
-    else if(board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1) {playerWin("c", 1);}
-    else if(board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1) {playerWin("c", 2);}
+    if(board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1) {playerWin("c", 0);return true;}
+    else if(board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1) {playerWin("c", 1);return true;}
+    else if(board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1) {playerWin("c", 2);return true;}
     //left - right
-    else if(board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1) {playerWin("r", 0);}
-    else if(board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1) {playerWin("r", 1);}
-    else if(board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1) {playerWin("r", 2);}
+    else if(board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1) {playerWin("r", 0);return true;}
+    else if(board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1) {playerWin("r", 1);return true;}
+    else if(board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1) {playerWin("r", 2);return true;}
     //diagonal
-    else if(board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) {playerWin("d", 0);}
-    else if(board[0][2] == 1 && board[1][1] == 1 && board[2][0] == 1) {playerWin("d", 1);}
+    else if(board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) {playerWin("d", 0);return true;}
+    else if(board[0][2] == 1 && board[1][1] == 1 && board[2][0] == 1) {playerWin("d", 1);return true;}
   
     //computer wins **********
     //up - down
-    else if(board[0][0] == 2 && board[0][1] == 2 && board[0][2] == 2) {computerWin("c", 0);}
-    else if(board[1][0] == 2 && board[1][1] == 2 && board[1][2] == 2) {computerWin("c", 1);}
-    else if(board[2][0] == 2 && board[2][1] == 2 && board[2][2] == 2) {computerWin("c", 2);}
+    else if(board[0][0] == 2 && board[0][1] == 2 && board[0][2] == 2) {computerWin("c", 0);return true;}
+    else if(board[1][0] == 2 && board[1][1] == 2 && board[1][2] == 2) {computerWin("c", 1);return true;}
+    else if(board[2][0] == 2 && board[2][1] == 2 && board[2][2] == 2) {computerWin("c", 2);return true;}
     //left - right
-    else if(board[0][0] == 2 && board[1][0] == 2 && board[2][0] == 2) {computerWin("r", 0);}
-    else if(board[0][1] == 2 && board[1][1] == 2 && board[2][1] == 2) {computerWin("r", 1);}
-    else if(board[0][2] == 2 && board[1][2] == 2 && board[2][2] == 2) {computerWin("r", 2);}
+    else if(board[0][0] == 2 && board[1][0] == 2 && board[2][0] == 2) {computerWin("r", 0);return true;}
+    else if(board[0][1] == 2 && board[1][1] == 2 && board[2][1] == 2) {computerWin("r", 1);return true;}
+    else if(board[0][2] == 2 && board[1][2] == 2 && board[2][2] == 2) {computerWin("r", 2);return true;}
     //diagonal
-    else if(board[0][0] == 2 && board[1][1] == 2 && board[2][2] == 2) {computerWin("d", 0);}
-    else if(board[0][2] == 2 && board[1][1] == 2 && board[2][0] == 2) {computerWin("d", 1);}
+    else if(board[0][0] == 2 && board[1][1] == 2 && board[2][2] == 2) {computerWin("d", 0);return true;}
+    else if(board[0][2] == 2 && board[1][1] == 2 && board[2][0] == 2) {computerWin("d", 1);return true;}
+    else{return false;}
   }
 }
 
 //c = column
 //r = row
 //d = diagonal
-void playerWin(char type, int loc){
+void playerWin(String type, int loc){
+  Serial.println("player win");
+  Serial.print("type = ");
+  Serial.println(type);
+  Serial.print("loc = ");
+  Serial.print(loc);
   //stop game
   turnIdentifier = 0;
   //turn off all lights
@@ -199,9 +299,11 @@ void playerWin(char type, int loc){
       }
     }
   }
+  turnIdentifier = 1;
 }
 
-void computerWin(char type, int loc){
+void computerWin(String type, int loc){
+  Serial.println("AI win");
   //stop game
   turnIdentifier = 0;
   //turn off all lights
@@ -261,5 +363,5 @@ void computerWin(char type, int loc){
       }
     }
   }
+  turnIdentifier = 1;
 }
-
